2c9e6d4df916898f78e76fbbf751a16553dd30a9
Mathieu Massicotte <38018243+massicottem@users.noreply.github.com>
2020-10-09 17:09:24 -0400

    Import old project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'massicottem'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('massicottem');
+    expect(app).toEqual('massicottem');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'massicottem';
+  ngOnInit(): void {
+    $(document).ready(function(){
+      $('.middle').hide();
+      $('nav').hide();
+      $('.nav1').hide();
+      $('.nav2').hide();
+      $('.nav3').hide();
+      $('.nav4').hide();
+      $('#all').hide().delay(500).fadeIn(2000, function(){
+        $('.middle').fadeIn(1000).delay(500, function(){
+          $('nav').fadeIn(1000, function(){
+            $('.nav4').fadeIn(700, function(){
+              $('.nav3').fadeIn(700, function(){
+                $('.nav2').fadeIn(700, function(){
+                  $('.nav1').fadeIn(700);
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  }
 }
